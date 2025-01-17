9385a7392ebc2ac82d79741480363d124c86c8a3
mc-cat-tty <44820563+mc-cat-tty@users.noreply.github.com>
2025-01-17 13:21:02 +0100

    Add exercise 1 plots

--- 6-planning/params.py
+++ 6-planning/params.py
@@ -2,15 +2,15 @@
 DT = 0.05  # time tick [s]
 ROBOT_RADIUS = 3.0  # robot radius [m]
 
-TARGET_SPEED = 10.0  # target speed [m/s]
+TARGET_SPEED = 15.0  # target speed [m/s]
 
 MAX_SPEED = 25.0  # maximum speed [m/s]
 MAX_ACCEL = 10.0  # maximum acceleration [m/ss]
 MAX_CURVATURE = 2.0  # maximum curvature [1/m]
-MAX_ROAD_WIDTH = 5.0  # maximum road width [m]
-D_ROAD_W = 0.5  # road width sampling length [m]
+MAX_ROAD_WIDTH = 4.0  # maximum road width [m]
+D_ROAD_W = 0.8  # road width sampling length [m]
 MAX_T = 3.0  # max prediction time [s]
-MIN_T = 2.5  # min prediction time [s]
+MIN_T = 2.7  # min prediction time [s]
 D_T_S = 0.5  # target speed sampling length [m/s]
 N_S_SAMPLE = 1  # sampling number of target speed
 
@@ -23,7 +23,7 @@ K_LON = 1.0
 
 
 # Figures rendering
-FIGS_PATH = "results/figures/ex3/speed-max/mpc-linear-st"
+FIGS_PATH = "results/figures/ex1/speed-2/shorter-execution"
 FIGS_NAMES = [
     "1-traj.png",
     "2-heading.png",
